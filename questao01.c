dc671a6a0d7f481e922b192add40c59b1c323ac5
hrq-araujo <37453824+hrq-araujo@users.noreply.github.com>
2023-04-10 16:26:07 -0300

    Add files via upload

---
+++ questao01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int INDICE = 13;
+    int SOMA = 0;
+    int K = 0;
+    
+    while(K < INDICE){
+      K = K + 1;
+      SOMA = SOMA + K;
+    }
+    printf("%d", SOMA);
+    
+    //o resultado de SOMA ao final do processamento é 91.
+}
